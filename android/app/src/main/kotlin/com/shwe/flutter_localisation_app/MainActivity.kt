ba36e2c49bef39723367e14e8d13ba99a75d58b0
Shwe-14 <theingishwesin4593@gmail.com>
2020-04-19 14:44:30 +0530

    Add tutorial for flutter localization

---
+++ android/app/src/main/kotlin/com/shwe/flutter_localisation_app/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.shwe.flutter_localisation_app
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
